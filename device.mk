5a2e2da583fde1b51c8c30ad8d03e9146f34d4dc
Golv <lok02@list.ru>
2020-09-08 12:26:04 +0300

    ocn: remove config from overlay-lineage to overlay-pe

--- device.mk
+++ device.mk
@@ -23,7 +23,7 @@ $(call inherit-product-if-exists, vendor/htc/ocn/ocn-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-pe
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
